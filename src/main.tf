3be8fde7eb43e9d617f000ad03ee7ac16f0cf43b
Javi Moreno <email@javier-moreno.com>
2023-10-04 08:22:02 +0000

    Reactivated profile

--- src/main.tf
+++ src/main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "workstation" {
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
 
-  # iam_instance_profile = "LabInstanceProfile"
+  iam_instance_profile = "LabInstanceProfile"
 
   user_data = file("userdata.sh")
 
