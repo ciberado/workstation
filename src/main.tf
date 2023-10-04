8f3299db8fc83182008738ea8b584c27d936655c
Javi Moreno <email@javier-moreno.com>
2023-10-04 07:59:32 +0000

    Using default vpc, without EIP

--- src/main.tf
+++ src/main.tf
@@ -1,21 +1,6 @@
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "${var.prefix}-vpc"
-  cidr = "${var.vpc_addr_prefix}.0.0/16"
-
-  azs = ["${var.region}${var.ws_az}"]
-  public_subnets = ["${var.vpc_addr_prefix}.101.0/24"]
-
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-
-  enable_nat_gateway = false
-
-  tags = {
-    Layer : "network fabric"
-  }
-}
+data "aws_vpc" "default" {
+  default = true
+} 
 
 resource "aws_security_group" "app_sg" {
   name        = "${var.prefix}_app_sg"
@@ -68,7 +53,7 @@ resource "aws_instance" "workstation" {
   vpc_security_group_ids      = [aws_security_group.app_sg.id]
   associate_public_ip_address = true
 
-  iam_instance_profile = "LabInstanceProfile"
+  # iam_instance_profile = "LabInstanceProfile"
 
   user_data = file("userdata.sh")
 
@@ -91,12 +76,3 @@ resource "aws_instance" "workstation" {
   }  
 }
 
-resource "aws_eip" "workstationip" {
-  instance = aws_instance.workstation.id
-  vpc      = true
-
-  tags = {
-    Name  : "${lower(var.owner)}-workstation"
-    Layer : "networking"
-  }  
-}
